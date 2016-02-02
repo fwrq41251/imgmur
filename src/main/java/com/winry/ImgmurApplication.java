fa7269955d151cc1922bbacf57c1aa123c29d4ef
congzhou <congzhou@wswincnhz0399.hengtiansoft.com>
2016-02-02 10:08:54 +0800

    init import

---
+++ src/main/java/com/winry/ImgmurApplication.java
@@ -0,0 +1,12 @@
+package com.winry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgmurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgmurApplication.class, args);
+	}
+}
